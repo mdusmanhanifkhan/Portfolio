f012ad54339016326f8be22137b19e086d61b175
Usman Khan <mdusmanhanifkhan@gmail.com>
2025-08-22 22:26:47 +0500

    start to design the home hero-section

---
+++ app/about-us/page.js
@@ -0,0 +1,7 @@
+export default function AboutUs(){
+    return(
+        <p className="text-white">
+        about us
+        </p>
+    )
+}
---
+++ app/components/hero-section/HeroSection.jsx
@@ -0,0 +1,20 @@
+
+
+export default function HeroSection() {
+  return (
+    <div className="flex flex-col gap-[20px] justify-center items-center  h-[60vh]">
+        <div className="font-bold text-[32px] font-spaceGrotesk text-white flex items-center gap-2 ">
+            Hi , i am <h1 className="text-green">Usman Hanif</h1>
+        </div>
+        <p className="text-white max-w-[677px] text-center font-inter">A passionate Fronted Software Developer 🚀 having a special interest in Frontend technologies and experience of building Web applications with JavaScript / Reactjs and some other cool libraries and frameworks</p>
+        <div>
+            <button>
+                contact me today
+            </button>
+            <button>
+                Contact me
+            </button>
+        </div>
+    </div>
+  )
+}
