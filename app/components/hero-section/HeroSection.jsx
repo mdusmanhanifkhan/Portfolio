f4cb94153cf9fee9e1c43e52559c591ad2fb29c8
Usman Khan <mdusmanhanifkhan@gmail.com>
2025-09-16 23:51:49 +0500

    initialize the breakpoint

--- app/components/hero-section/HeroSection.jsx
+++ app/components/hero-section/HeroSection.jsx
@@ -1,23 +1,25 @@
 import Button from "../other/button/Button";
 
-
 export default function HeroSection() {
   return (
-    <div className="flex flex-col gap-[20px] justify-center items-center h-[60vh] relative">
-        <div className="border-[50px] blur-2xl bg-[#D9D9D905] rounded-full w-120 h-120 absolute top-50 left-1/2 -translate-x-1/2 z-0">
+    <div className="flex flex-col gap-[30px] justify-center items-center h-[60vh] relative ">
+      <div className="flex flex-col gap-[16px] justify-center items-center z-10 relative">
+        <div className="font-bold text-[24px] tablet:text-[28px] laptop:text-[28px] desktop:text-[26px] desktop-lg:text-[28px] desktop-xl:text-[32px] font-spaceGrotesk text-white flex items-center gap-2 ">
+          Hi , i am <h1 className="text-green">Usman Hanif</h1>
         </div>
-        <div className="font-bold text-[32px] font-spaceGrotesk text-white flex items-center gap-2 ">
-            Hi , i am <h1 className="text-green">Usman Hanif</h1>
-        </div>
-        <p className="text-white max-w-[677px] text-center font-inter">A passionate Fronted Software Developer 🚀 having a special interest in Frontend technologies and experience of building Web applications with JavaScript / Reactjs and some other cool libraries and frameworks</p>
-        <div className="flex items-center gap-[15px] relative z-10">
-            <Button variant={'bgBtn'}>
-                Contact me Today
-            </Button>
-            <Button className={''} variant={'outlineBtn'}>
-                Contact me
-            </Button>
-        </div>
+        <p className="text-white max-w-[677px] text-center font-inter text-[12px] laptop:text-[14px]">
+          A passionate Fronted Software Developer 🚀 having a special interest
+          in Frontend technologies and experience of building Web applications
+          with JavaScript / Reactjs and some other cool libraries and frameworks
+        </p>
+      </div>
+
+      <div className="flex items-center gap-[15px] relative z-10">
+        <Button variant={"bgBtn"}>Contact me Today</Button>
+        <Button className={""} variant={"outlineBtn"}>
+          Contact me
+        </Button>
+      </div>
     </div>
-  )
+  );
 }
--- app/page.js
+++ app/page.js
@@ -1,11 +1,15 @@
 import HeroSection from "./components/hero-section/HeroSection";
+import Container from "./components/other/layout/Container";
 
 export default function Home() {
   return (
-    <div className="relative">
+    <div className="relative overflow-x-clip">
       <div className="absolute top-[-146px] left-[-162px] w-[377px] h-[377px] bg-green-200 rounded-full blur-2xl"></div>
       <div className="absolute top-[-296px] left-[893px] w-[492px] h-[492px] bg-green-200 rounded-full blur-2xl"></div>
-      <HeroSection/>
+      <div className="border-[50px] blur-2xl bg-[#D9D9D905] rounded-full w-120 h-120 absolute top-50 left-1/2 -translate-x-1/2 z-0"></div>
+      <Container>
+        <HeroSection />
+      </Container>
     </div>
   );
 }
